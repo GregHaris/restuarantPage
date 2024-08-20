e0f4445dca152fed1595cb00d5f6a7db780e8aa9
Grëg Häris <d.gregharis@gmail.com>
2024-08-20 10:31:11 +0100

    create the initial load function. this file handles the initial page loading

---
+++ src/pageLoad.js
@@ -0,0 +1,9 @@
+import createRestaurantHomePage from "./homepage";
+import createTabs from "./tabs";
+
+function initialLoad() {
+  createTabs();
+  createRestaurantHomePage();
+}
+
+export default initialLoad;
