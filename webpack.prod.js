b704ade4c46eb6690007dd3a3738985ae0132390
Grëg Häris <d.gregharis@gmail.com>
2024-08-21 13:57:57 +0100

    add source-map devtool and MiniCssExtractPlugin loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,14 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 });
