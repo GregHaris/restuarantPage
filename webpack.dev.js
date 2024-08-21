3528d356f0882f257ab111b04a593e41e1105dd5
Grëg Häris <d.gregharis@gmail.com>
2024-08-21 13:18:33 +0100

    split the configuration code between the three files according to function

---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    watchFiles: ["./src/template.html"], 
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+});
