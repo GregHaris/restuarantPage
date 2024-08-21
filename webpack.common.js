f3e62e48b2dc44d33044f0359ad0a21738174534
Grëg Häris <d.gregharis@gmail.com>
2024-08-21 13:42:12 +0100

    modified the webpack settings files: moved module rules from .dev.js to .common.js as .prod.js when called 'npm run build' needs it to bundle the css
    used prittier to format

--- webpack.common.js
+++ webpack.common.js
@@ -4,11 +4,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: {
     app: "./src/index.js",
-  }, 
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-      title: "production"
+      title: "production",
     }),
   ],
   output: {
@@ -16,5 +16,16 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,23 +1,11 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"], 
-    static: './dist',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpeg|jpg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
+    watchFiles: ["./src/template.html"],
+    static: "./dist",
   },
 });
